a084aeb5b2057ae13f9e8e9c0b207fa1f26c5cc5
acce8711 <101413675+acce8711@users.noreply.github.com>
2026-03-31 00:30:03 -0400

    updated models

---
+++ Assets/Scenes/Scripts/Cottage/MusicPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource music_player;
+    private bool is_music_playing;
+
+    private void Start()
+    {
+        is_music_playing = true;
+        music_player.Play();
+        gameObject.transform.DORotate(new Vector3(0, 360, 0), 5f, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
+    }
+
+    public void ToggleMusic()
+    {
+        if (is_music_playing)
+        {
+            music_player.Play();
+            gameObject.transform.DORotate(new Vector3(0, 360, 0), 5f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
+        }
+        else
+        {
+            music_player.Pause();
+            gameObject.transform.DOKill();
+        }
+
+        is_music_playing = !is_music_playing;
+    }
+}
